8e230376e6797e59449f1b6f526e72e882ef21da
idrisatijjani41-png <idrisatijjani41@gmail.com>
2026-08-21 11:31:20 +0100

    Add settings.gradle.kts for Flutter and Android setup

---
+++ android/settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = settingsDir.resolve("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        val flutterSdk = properties.getProperty("flutter.sdk")
+        assert(flutterSdk != null) { "flutter.sdk not set in local.properties" }
+        flutterSdk
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+include(":app")

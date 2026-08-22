d90d00b41423a1d15e31697bffa1a72b9c2753ae
idrisatijjani41-png <idrisatijjani41@gmail.com>
2026-08-22 12:48:36 +0100

    Update Flutter SDK path loading and plugin versions

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,10 @@
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        val localPropertiesFile = settingsDir.resolve("local.properties")
-        if (localPropertiesFile.exists()) {
-            localPropertiesFile.inputStream().use { properties.load(it) }
-        }
-        val flutterSdk = properties.getProperty("flutter.sdk")
-        assert(flutterSdk != null) { "flutter.sdk not set in local.properties" }
-        flutterSdk
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -21,9 +18,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
